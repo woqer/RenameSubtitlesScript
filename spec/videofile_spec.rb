b1faf17280b7564751adaa2d64324232bda8fc8b
Wilfrido Vidana <woqer88@gmail.com>
2015-09-16 20:20:39 -0500

    added tests for patterns constants

--- spec/videofile_spec.rb
+++ spec/videofile_spec.rb
@@ -5,10 +5,34 @@ describe VideoFile do
   let(:videofile_class) { Class.new { include VideoFile } }
   let(:videofile) { videofile_class.new }
 
+  describe "#episode_patterns_lvl1" do
+    context "when calling method" do
+      it "returns array of episode_patterns_lvl1" do
+        expect(videofile.episode_patterns_lvl1).to eq [/S\d+E\d+/, /\d+x\d+/, /.*\d+/]
+      end
+    end
+  end
+
+  describe "#episode_patterns_lvl2" do
+    context "when calling method" do
+      it "returns array of episode_patterns_lvl2" do
+        expect(videofile.episode_patterns_lvl2).to eq [/E\d+/, /x\d+/, /\d+/]
+      end
+    end
+  end
+
   describe "#video_extensions" do
     context "when calling method" do
       it "returns array of video extensions" do
         expect(videofile.video_extensions).to eq ["mkv", "avi", "mp4"]
+      end
+    end
+  end
+
+  describe "#subtitle_extensions" do
+    context "when calling method" do
+      it "returns array of subtitle extensions" do
+        expect(videofile.subtitle_extensions).to eq ["srt", "sub"]
       end
     end
   end
